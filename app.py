3c6d96df93d0f64a74563a86946b2549575056a1
Travis Rollins <kalikoze@gmail.com>
2024-04-16 17:40:20 -0700

    Create an deck of 52 cards listed as tuples

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+def createDeck():
+  suits = ['♠', '♡', '♢', '♣']
+  ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  deck = [(suit, rank) for suit in suits for rank in ranks]
+  return deck
+
+deck = createDeck()
+print(deck)
+
 def printInstructions():
     print("Welcome to Text-based Blackjack!")
     print("Instructions:")
