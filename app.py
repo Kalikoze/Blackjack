66df6e172373573fa491c96c07a7bb478e6fba99
Travis Rollins <kalikoze@gmail.com>
2024-04-16 17:32:03 -0700

    Initial commit that executes basic instructions for blackjack game

---
+++ app.py
@@ -0,0 +1,11 @@
+def printInstructions():
+    print("Welcome to Text-based Blackjack!")
+    print("Instructions:")
+    print("- Type 'hit' to draw another card")
+    print("- Type 'stand' to keep your current hand")
+
+def startGame():
+    printInstructions()
+
+
+startGame()
